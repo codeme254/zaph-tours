04f0726cd717634d282045bc4b05d14b2419e7a9
otwoma <otwomadennis365@gmail.com>
2022-04-15 09:09:17 +0300

    progress on the tours section

--- src/Components/Testimonials/Testimony.jsx
+++ src/Components/Testimonials/Testimony.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Suitcase } from "../Icons/Icons";
 
 const Testimony = ({userProfileImage, name, date, testimonyTitle, testimony }) => {
     return(
         <div className="testimony" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, .2), rgba(0, 0, 0, .3)),url(${userProfileImage})`}}>
+                <div className="testimony__icon">
+                    <Suitcase />
+                </div>
             <div className="testimony__body">
                 <div className="testimony__name-date">
                     <p className="testimony__name-date--name">{name}</p>
